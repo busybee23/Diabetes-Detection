9b8195d9c97082accd0df92d25766fd3df9b3897
Yash Chaudhary <chaudharyyash088@gmail.com>
2020-10-28 22:34:10 +0530

    added index.html

--- Flask Integration/app.py
+++ Flask Integration/app.py
@@ -1,40 +1,63 @@
-from flask import Flask, render_template, flash, request
-from get_model import *
+import numpy as np 
+from flask import Flask, request, render_template
+import pickle 
 
-#initializing the model
-MODEL_PATH = "models/logistic_reg.sav"
-model = LoadModel(MODEL_PATH)
-
-# App config.
-DEBUG = True
 app = Flask(__name__)
-app.config.from_object(__name__)
-app.config['SECRET_KEY'] = '7d441f27d441f27567d441f2b6176a'
+model = pickle.load(open('model.pkl','rb'))
 
 #Define home route
 @app.route("/")
-def index():
+def home():
     return render_template("index.html")
 
 #Define diagnosis route
 @app.route("/diagnosis", methods=['POST'])
 def diagnosis():
-    name = request.form['name']
-    age = request.form['age']
-    pregnant = request.form['pregnant']
-    insulin = request.form['insulin']
-    bmi = request.form['bmi']
-    pedigree = request.form['pedigree']
-    glucose = request.form['glucose']
-    bp = request.form['bp']
-    #Predict on the given parameters
-    prediction = model.predict_class(pregnant,insulin,bmi,age,glucose,bp,pedigree)
-    print(prediction)
-    #Route for result
-    if prediction[0] == '1':
-        return render_template("positive.html", result="true")
-    elif prediction[0] == '0':
-        return render_template("negetive.html", result="true")
-        
+    '''
+    For rendering results on HTML GUI 
+    '''
+    int_features = [float(x) for x in request.form.values()]
+    final_features = [np.array(int_features)]
+    prediction = model.predict(final_features)
+
+    
+
+    if prediction == 1 :
+        return render_template('index.html', prediction_text="Positive")
+    else :
+        return render_template('index.html', prediction_text="Negative")
+
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+#     name = request.form['name']
+#     age = request.form['age']
+#     pregnant = request.form['pregnant']
+#     insulin = request.form['insulin']
+#     bmi = request.form['bmi']
+#     pedigree = request.form['pedigree']
+#     glucose = request.form['glucose']
+#     bp = request.form['bp']
+#     #Predict on the given parameters
+#     prediction = model.predict_class(pregnant,insulin,bmi,age,glucose,bp,pedigree)
+#     print(prediction)
+#     #Route for result
+#     if prediction[0] == '1':
+#         return render_template("positive.html", result="true")
+#     elif prediction[0] == '0':
+#         return render_template("negetive.html", result="true")
+        
+# # if __name__ == "__main__":
+# #     app.run()
