97c777d302330b75166c9384f6614b25984b80a6
Yash Chaudhary <chaudharyyash088@gmail.com>
2020-10-29 22:19:13 +0530

    update

--- Flask Integration/app.py
+++ Flask Integration/app.py
@@ -34,30 +34,3 @@ if __name__ == "__main__":
     
     
     
-    
-    
-    
-    
-    
-    
-    
-    
-#     name = request.form['name']
-#     age = request.form['age']
-#     pregnant = request.form['pregnant']
-#     insulin = request.form['insulin']
-#     bmi = request.form['bmi']
-#     pedigree = request.form['pedigree']
-#     glucose = request.form['glucose']
-#     bp = request.form['bp']
-#     #Predict on the given parameters
-#     prediction = model.predict_class(pregnant,insulin,bmi,age,glucose,bp,pedigree)
-#     print(prediction)
-#     #Route for result
-#     if prediction[0] == '1':
-#         return render_template("positive.html", result="true")
-#     elif prediction[0] == '0':
-#         return render_template("negetive.html", result="true")
-        
-# # if __name__ == "__main__":
-# #     app.run()
